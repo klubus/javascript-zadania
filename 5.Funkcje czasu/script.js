8e3996e88a9ca46ea0816a30603716009311d2a5
klubus <72337504+klubus@users.noreply.github.com>
2021-02-04 20:42:03 +0100

    Update script.js

---
+++ 5.Funkcje czasu/script.js
@@ -0,0 +1,26 @@
+// zadanie 1
+let time = 0;
+const interval = setInterval(() => {
+    time += 1;
+    console.log(`Cześć po raz ${time}`);
+    if(time >= 15) {
+        clearInterval(interval);
+    }
+}, 1000);
+
+// zadanie 2
+let myArray = [1, 2, 3, 4];
+let myTimer = setTimeout(() => {
+    for (let i=0; i < myArray.length; i++) {
+        console.log (myArray[i]);
+    }
+    let j = 0
+    let myInterval = setInterval(() => {
+        console.log (myArray[j]);
+        j++
+        if (j >= myArray.length) {
+            clearInterval(myInterval);
+        }
+    }, 3000)
+    
+}, 2000 )
