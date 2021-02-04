77b956a6dd45e7ae48deab5d54395f9dd7578307
klubus <72337504+klubus@users.noreply.github.com>
2021-02-04 20:29:38 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,46 @@
+// zadanie 1
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 === bool2);
+
+// zadanie 2
+let num1=1;
+let num2=2;
+
+let moduloResult=0;
+
+moduloResult=num1%num2;
+
+console.log(moduloResult);
+
+// zadanie 3
+let txt1 = "abc";
+let txt2 = "xyz";
+let stringsResult = "";
+
+stringsResult = txt1+txt2;
+
+console.log(stringsResult);
+
+// zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+// zadanie 5
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+// zadanie 6
+let num3 = 12;
+let num4 = 5;
+let result = 0;
+
+result = num3 > num4;
+console.log(result);
