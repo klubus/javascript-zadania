c5b727c54bd721ff2ece4c9a67498e6e5a40be6a
klubus <72337504+klubus@users.noreply.github.com>
2021-02-04 20:17:49 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+// Zadanie 1:
+
+console.log("Krystian Kluba");
+
+// Zadanie 2:
+
+console.log(12 +32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// Zadanie 3:
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+// 13 - wyświetla string "Hello World"
+// 14 - Syntax error - brak znaków "
+// 15 - Syntax error - brak znaku "
